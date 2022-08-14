3d08cee4d46afc33afd9b7035295c2426af06170
Kelvin Mutua Kilonzo <kelvinmutua959@gmail.com>
2022-08-14 17:33:38 -0500

    edited challenge_logic.js to clean up line 17

--- static/js/challenge_logic.js
+++ static/js/challenge_logic.js
@@ -14,7 +14,7 @@ let satelliteStreets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/sate
 	maxZoom: 18,
 	accessToken: API_KEY
 });
-  v
+
 // Deliverable 3: Add more layers 
 let light = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/{z}/{x}/{y}?access_token={accessToken}',{
     attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
